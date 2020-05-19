746c8ddf2022e9259da486bb2f5070ed7107a797
tony <ashaid2@lsu.edu>
2020-05-18 23:57:31 -0500

    uh fixing things

--- face/views.py
+++ face/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib import messages
+from django.utils import timezone
 
 
 # home page
 def home(request):
     # messages.success(request, f'image chosen successfully')
 
-    return render(request, 'face/home.html')
+    image = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'face/home.html', {'image': image})
 
 
 # about page
