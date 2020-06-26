f0b6633af18b2ee92bbb69e889192139b5c3bd13
tony <ashaid2@lsu.edu>
2020-06-26 01:19:06 -0700

    updated settings to allow deployment on heroku

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+from whitenoise import WhiteNoise
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+
+application = WhiteNoise(get_wsgi_application())
+
