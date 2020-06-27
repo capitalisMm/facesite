e28fccbd7249e03c00924b24fef9d13f9b7730a0
tony <ashaid2@lsu.edu>
2020-06-26 20:49:28 -0700

    added stuff

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -11,9 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
-application = WhiteNoise(get_wsgi_application())
+application = (get_wsgi_application())
 
