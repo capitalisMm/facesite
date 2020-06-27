3b3c9630765a1bb4f5772a126da3e425d447bf94
tony <ashaid2@lsu.edu>
2020-06-26 20:51:14 -0700

    removed whitenoise

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['friend-facecog.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'face.apps.FaceConfig',
     'crispy_forms',
     'django.contrib.admin',
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
