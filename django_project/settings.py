43f2923eb7a5d7ecf87a1fd8a005d05e711bf5b8
tony <ashaid2@lsu.edu>
2020-06-28 00:59:43 -0700

    I FORGOT TO ADD a 1 TO ALLOWED HOSTS IM STUPID

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['67.172.133.251']
+ALLOWED_HOSTS = ['167.172.133.251']
 
 
 # Application definition
